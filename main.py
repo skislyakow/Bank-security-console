a9f4e10ee4cefac1db9eeb01ca6672ff1460bfed
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-01-17 16:55:13 +0300

    remove junk code

--- main.py
+++ main.py
@@ -7,26 +7,8 @@ django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
 
-if __name__ == '__main__':
-    # Программируем здесь
-    
-
-    #print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-    #print(Passcard.objects.all())
-    
+if __name__ == '__main__':    
     passcards = Passcard.objects.all()
-    #print('owner_name: ', passcards[0].owner_name)
-    #print('passcode: ', passcards[0].passcode)
-    #print('created_at: ', passcards[0].created_at)
-    #print('is_active: ', passcards[0].is_active)
-
-    
-
-    #active_passcards = []
-    #for passcard in passcards:        
-        #if passcard.is_active:
-            #active_passcards.append(passcards)
-
     active_passcards = Passcard.objects.filter(is_active=True)
 
     print('Всего пропусков: ', len(passcards))
